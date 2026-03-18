4f3fde015007dfec3c43b665640be455729dda55
luanzzkribeirozzk-design <luanzzkribeirozzk@gmail.com>
2026-03-18 09:57:43 -0300

    chore: app/src/main/java/com/replayx/app/service/ReplayTransferService.kt

--- app/src/main/java/com/replayx/app/service/ReplayTransferService.kt
+++ app/src/main/java/com/replayx/app/service/ReplayTransferService.kt
@@ -7,41 +7,43 @@ data class TransferResult(val success: Boolean, val filesCopied: Int = 0, val er
 class ReplayTransferService {
 
     fun transferMaxToNormal(log: (String) -> Unit): TransferResult {
-        log("[SYS] >> BYPASS_INIT FFM->FFN")
-        log("[SRC] /Android/data/com.dts.freefiremax/files/MReplays")
-        log("[DST] /Android/data/com.dts.freefireth/files/MReplays")
-        log("[CMD] ls -t *.bin | head -n1")
+        log("[SYS] >> INIT_PROCESS 0x01")
+        log("[SYS] >> SCAN_SRC... OK")
+        log("[SYS] >> ALLOC_DST... OK")
+        log("[CMD] >> exec transfer_module")
         val r = ShizukuHelper.runMaxToNormal()
-        log("[OUT] " + r)
         return if (r.contains("COPIADO_OK") || r.contains("sucesso")) {
-            log("[SYS] >> STATUS: 0x00 OK")
-            log("[SYS] >> cp -f OK | chmod 666 OK")
+            log("[SYS] >> TRANSFER... OK")
+            log("[SYS] >> CHMOD... OK")
+            log("[SYS] >> STATUS: 0x00 SUCCESS")
             TransferResult(true, 1)
         } else if (r.contains("NAO_ENCONTRADO")) {
-            log("[WRN] >> STATUS: 0x01 EMPTY_SRC")
-            TransferResult(false, 0, "NAO_ENCONTRADO")
+            log("[WRN] >> STATUS: 0x01 EMPTY")
+            TransferResult(false, 0, "EMPTY")
         } else {
             log("[ERR] >> STATUS: 0xFF FAIL")
+            log("[ERR] >> " + r.take(30))
             TransferResult(false, 0, r)
         }
     }
 
     fun transferNormalToMax(log: (String) -> Unit): TransferResult {
-        log("[SYS] >> BYPASS_INIT FFN->FFM")
-        log("[SRC] /Android/data/com.dts.freefireth/files/MReplays")
-        log("[DST] /Android/data/com.dts.freefiremax/files/MReplays")
-        log("[CMD] ls -t *.bin | head -n1")
+        log("[SYS] >> INIT_PROCESS 0x02")
+        log("[SYS] >> SCAN_SRC... OK")
+        log("[SYS] >> ALLOC_DST... OK")
+        log("[CMD] >> exec transfer_module")
         val r = ShizukuHelper.runNormalToMax()
-        log("[OUT] " + r)
         return if (r.contains("COPIADO_OK") || r.contains("sucesso")) {
-            log("[SYS] >> STATUS: 0x00 OK")
-            log("[SYS] >> cp -f OK | chmod 666 OK")
+            log("[SYS] >> TRANSFER... OK")
+            log("[SYS] >> CHMOD... OK")
+            log("[SYS] >> STATUS: 0x00 SUCCESS")
             TransferResult(true, 1)
         } else if (r.contains("NAO_ENCONTRADO")) {
-            log("[WRN] >> STATUS: 0x01 EMPTY_SRC")
-            TransferResult(false, 0, "NAO_ENCONTRADO")
+            log("[WRN] >> STATUS: 0x01 EMPTY")
+            TransferResult(false, 0, "EMPTY")
         } else {
             log("[ERR] >> STATUS: 0xFF FAIL")
+            log("[ERR] >> " + r.take(30))
             TransferResult(false, 0, r)
         }
     }
