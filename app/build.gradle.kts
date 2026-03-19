bf22872f78b46d3b6bf847d00139f61be4ce75b3
luanzzkribeirozzk-design <luanzzkribeirozzk@gmail.com>
2026-03-19 00:15:29 -0300

    chore: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,5 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
-    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-firestore")
 }
