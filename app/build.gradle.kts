8cea276fe3c278423f42eb83fc87609c069748e9
luanzzkribeirozzk-design <luanzzkribeirozzk@gmail.com>
2026-03-20 13:23:39 -0300

    chore: app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,15 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
